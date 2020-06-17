d66e0b65c95b382066044f2ea4e4b962ac627efc
LaithTahboub <59159345+LaithTahboub@users.noreply.github.com>
2020-06-17 09:22:12 +0300

    Update main.cpp

--- Chess/main.cpp
+++ Chess/main.cpp
@@ -24,12 +24,28 @@ piece::piece(vector<pair<int, int>> x, char y, pair<int, int> z) {
     location = z;
 }
 
+piece Wpawn1({{0, 1}, {0, 2}}, 'P', {0, 1});
+piece Wpawn2({{1, 1}, {1, 1}}, 'P', {1, 1});
+piece Wpawn3({{2, 1}, {2, 2}}, 'P', {2, 1});
+piece Wpawn4({{3, 1}, {3, 2}}, 'P', {3, 1});
+piece Wpawn5({{4, 1}, {4, 2}}, 'P', {4, 1});
+piece Wpawn6({{5, 1}, {5, 2}}, 'P', {5, 1});
+piece Wpawn7({{6, 1}, {6, 1}}, 'P', {6, 1});
+piece Wpawn8({{7, 1}, {7, 1}}, 'P', {7, 1});
+piece Wbishop1({}, 'B', {3, 0});
+piece Wbishop2({}, 'B');
+piece WKnight1;
+piece WKnight
+
 void draw() {
 
 }
 
+void input() {
+
+}
+
 void logic() {
-    piece Knight1();
 }
 
 int main() {
